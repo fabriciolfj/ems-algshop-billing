5ca0f4440fb09f19a5b8d077b415c90fd28f95ba
fabricio <fabricio.jacob@outlook.com>
2026-06-09 21:18:58 -0300

    feat: add

---
+++ src/main/java/com/algaworks/algashop/billing/infrastructure/payment/AlgaShopPaymentProperties.java
@@ -0,0 +1,37 @@
+package com.algaworks.algashop.billing.infrastructure.payment;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Component
+@Data
+@Validated
+@ConfigurationProperties(prefix = "algashop.integrations.payment")
+public class AlgaShopPaymentProperties {
+
+    @NotNull
+    private AlgaShopPaymentProvider provider;
+
+    @NotNull
+    private FastpayProperties fastpay;
+
+    public enum AlgaShopPaymentProvider {
+        FAKE,
+        FASTPAY
+    }
+
+    @Validated
+    @Data
+    public static class FastpayProperties {
+        @NotBlank
+        private String hostname;
+
+        @NotBlank
+        private String privateToken;
+
+    }
+}
