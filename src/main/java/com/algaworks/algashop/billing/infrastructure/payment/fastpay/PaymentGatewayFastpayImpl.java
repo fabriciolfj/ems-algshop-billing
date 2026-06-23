8babef48819651a969f6293d08feb43a60ac9b21
fabricio <fabricio.jacob@outlook.com>
2026-06-22 22:00:16 -0300

    feat: add

--- src/main/java/com/algaworks/algashop/billing/infrastructure/payment/fastpay/PaymentGatewayFastpayImpl.java
+++ src/main/java/com/algaworks/algashop/billing/infrastructure/payment/fastpay/PaymentGatewayFastpayImpl.java
@@ -1,22 +1,87 @@
 package com.algaworks.algashop.billing.infrastructure.payment.fastpay;
 
+import com.algaworks.algashop.billing.domain.model.creditcard.CreditCardNotFoundException;
+import com.algaworks.algashop.billing.domain.model.creditcard.CreditCardRepository;
 import com.algaworks.algashop.billing.domain.model.invoice.payment.Payment;
 import com.algaworks.algashop.billing.domain.model.invoice.payment.PaymentGatewayService;
 import com.algaworks.algashop.billing.domain.model.invoice.payment.PaymentRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
+@RequiredArgsConstructor
 @ConditionalOnProperty(name = "algashop.integrations.payment.provider", havingValue = "FASTPAY")
 public class PaymentGatewayFastpayImpl implements PaymentGatewayService {
 
+    private final FastpayPaymentApiClient fastpayPaymentApiClient;
+    private final CreditCardRepository creditCardRepository;
+
     @Override
     public Payment capture(PaymentRequest request) {
-        return null;
+        final var input = convertInput(request);
+        final var response = fastpayPaymentApiClient.capure(input);
+        return convertToPayment(response);
     }
 
     @Override
     public Payment findByCode(String gatewayCode) {
-        return null;
+        final var response = fastpayPaymentApiClient.findById(gatewayCode);
+        return convertToPayment(response);
+    }
+
+    private Payment convertToPayment(FastpayPaymentModel response) {
+        var builder = Payment.builder()
+                .gatewayCode(response.getId())
+                .invoiceId(UUID.fromString(response.getReferenceCode()));
+
+        FastpayPaymentMethod fastpayPaymentMethod;
+        try {
+            fastpayPaymentMethod = FastpayPaymentMethod.valueOf(response.getMethod());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Unknown payment method: " + response.getMethod());
+        }
+
+        FastpayPaymentStatus fastpayPaymentStatus;
+        try {
+            fastpayPaymentStatus = FastpayPaymentStatus.valueOf(response.getStatus());
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Unknown payment status: " + response.getStatus());
+        }
+
+        builder.method(FastpayEnumConverter.convert(fastpayPaymentMethod));
+        builder.status(FastpayEnumConverter.convert(fastpayPaymentStatus));
+        return builder.build();
+    }
+
+    private FastpayPaymentInput convertInput(PaymentRequest request) {
+        var payer = request.getPayer();
+        var address = payer.getAddress();
+
+        var builder = FastpayPaymentInput.builder()
+                .totalAmount(request.getAmount())
+                .referenceCode(request.getInvoiceId().toString())
+                .fullName(payer.getFullName())
+                .document(payer.getDocument())
+                .phone(payer.getPhone())
+                .zipCode(address.getZipCode())
+                .addressLine1(address.getStreet() + ", " + address.getNumber())
+                .addressLine2(address.getComplement())
+                .replyToUrl("http://example.com/webhook");
+
+        switch (request.getMethod()) {
+            case CREDIT_CARD -> {
+                builder.method(FastpayPaymentMethod.CREDIT.name());
+                var creditCard = creditCardRepository.findById(request.getCreditCardId())
+                        .orElseThrow(CreditCardNotFoundException::new);
+                builder.creditCardId(creditCard.getGatewayCode());
+            }
+            case GATEWAY_BALANCE -> {
+                builder.method(FastpayPaymentMethod.GATEWAY_BALANCE.name());
+            }
+        }
+        return builder.build();
     }
 }
